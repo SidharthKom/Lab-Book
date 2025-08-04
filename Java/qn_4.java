23626176df66e20d381478523f199775daa32a12
Sidharth K <90872355+SidharthKom@users.noreply.github.com>
2025-08-04 10:29:15 +0530

    Create qn_4.java

---
+++ Java/qn_4.java
@@ -0,0 +1,15 @@
+package Lab_1;
+public class qn_4 {
+	public static void main(String[] args) {
+		System.out.println("Personal Details: ");
+		System.out.println("___________________");
+
+		System.out.println("\nFirst Name: Sidharth");
+		System.out.println("Last Name: K");
+		System.out.println("Gender: M");
+		System.out.println("Age: 23");
+		System.out.println("Weight: 63");
+	}
+	
+
+}
