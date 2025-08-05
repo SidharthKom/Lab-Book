9770e3002911ff4f9481242a9ce49bb5185d6159
SidharthKomm <siddukommadath@gmail.com>
2025-08-05 06:47:04 +0530

    w

--- src/test/java/Lab_1/qn_11.java
+++ src/test/java/Lab_1/qn_11.java
@@ -1,73 +1,28 @@
 package Lab_1;
 
+import java.util.Scanner;
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
-	 
-public class qn_11 {
-		String firstName;
-		String lastName;
-		char gender;
-		LocalDate dob;
-		
-		qn_11(String firstName,String lastName,char gender,LocalDate dob){
-		this.firstName =firstName;
-		this.lastName = lastName;
-		this.gender = gender;
-		this.dob =dob;
-		}
-		
-		public String getFirstName() {
-			return firstName;
-		}
 
-		public void setFirstName(String firstName) {
-			this.firstName = firstName;
-		}
+public class qn_11{
 
-		public String getLastName() {
-			return lastName;
-		}
+    public static void main(String[] args) {
+    	
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        System.out.println("Enter the date");
+        String a = sc.nextLine();
+        LocalDate date = LocalDate.parse(a, formatter);
+        LocalDate curDate = LocalDate.now();
+        Period dur = Period.between(date, curDate);
+        
+        System.out.println("The Duration is: ");
+        System.out.println("Years  : " +dur.getYears());
+        System.out.println("Months : " + dur.getMonths());
+        System.out.println("Days   : " +dur.getDays());
 
-		public void setLastName(String lastName) {
-			this.lastName = lastName;
-		}
-
-		public char getGender() {
-			return gender;
-		}
-
-		public void setGender(char gender) {
-			this.gender = gender;
-		}
-		public int calculateAge(LocalDate dob) {
-			LocalDate currentDate = LocalDate.now();
-			return Period.between(dob, currentDate).getYears();
-		}
-		public String getFullName(String firstName,String lastName) {
-			return firstName+" "+lastName;
-		}
-		
-		void display() {
-			System.out.println("Personal Details: ");
-			System.out.println("___________________\n");
-			System.out.println("Full Name: "+ getFullName(getFirstName(),getLastName()));
-			System.out.println("Age: "+ calculateAge(dob));
-			System.out.println("gender: "+ getGender());
-			
-		}
-		public static void main(String[] args) {
-			LocalDate dob = LocalDate.of(2002, 05, 02);
-			qn_11 obj = new qn_11("Sidharth","K",'M',dob);
-			
-			obj.display();
-			
-		}
-		
-		
-	}
-	
-	 
-	 
-	
-
+        
+    }
+}
